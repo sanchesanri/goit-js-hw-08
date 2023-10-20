6da67cfbf200c26b9cfa9bd9ff1a8d4685fe807f
AnriSanches <89216270+sanchesanri@users.noreply.github.com>
2023-10-20 15:41:39 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import { values } from '@vimeo/player';
 
+const iframeRef = document.querySelector('#vimeo-player');
+
+const iframeId = iframeRef.id
+
+const player = new Player(iframeRef);
+
+const onPlay = function({seconds}) {
+    localStorage.setItem("videoPlayer-current-time", seconds);
+    console.log(JSON.parse(localStorage.getItem("videoPlayer-current-time")));
+};
+
+
+
+player.on('timeupdate', onPlay);
+
+const getApplicationVAlue = JSON.parse(localStorage.getItem("videoPlayer-current-time")) ??0; 
+console.log(getApplicationVAlue);
+
+player.setCurrentTime(getApplicationVAlue).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
